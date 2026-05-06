0414fca520d83df5ec65f4154b0249997feadecf
moti_kina <seiyabb0414@gmail.com>
2026-05-06 21:45:36 +0900

    プレイヤーを追うスクリプト

--- Assets/script/enemy follow.cs
+++ Assets/script/enemy follow.cs
@@ -2,15 +2,25 @@ using UnityEngine;
 
 public class enemyfollow : MonoBehaviour
 {
+    public float movespeed=2f;
+    private Transform playerTransform;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        GameObject player=GameObject.FindGameObjectWithTag("Player");
+        if(player!=null)
+        {
+            playerTransform=player.transform;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(playerTransform!=null)
+        {
+            Vector2 direction=(playerTransform.position-transform.position).normalized;
+            transform.Translate(direction*movespeed*Time.deltaTime);
+        }
     }
 }
